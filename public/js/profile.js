2e0a9ec9f16011f5c807b8dc1c03f8328d72ad9d
Stefan Deak <deakstefan@gmail.com>
2019-02-15 11:53:02 +0100

    Implement delete user

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,1 +1,19 @@
-console.log('radi');
+document.getElementById('delete-account').addEventListener("click", deleteAccount);
+
+let responseField = document.getElementById('register-response');
+
+function deleteAccount() {
+    if (confirm('Delete account?')) {
+        axios.delete('/users')
+            .then((response) => {
+                responseField.innerHTML = response.data.text;
+                setTimeout(() => {
+                    window.location.assign('/');
+                }, 1000);
+            })
+            .catch((err) => {
+                responseField.innerHTML = err.response.data;
+            });
+    };
+
+};
