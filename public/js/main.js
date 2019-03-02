7b3a54e98fd22fa1f724582db07540731649073a
Miljan Rubezic <miljanrubezic@gmail.com>
2019-03-02 23:17:16 +0100

    Fixed script issues

--- public/js/main.js
+++ public/js/main.js
@@ -43,7 +43,9 @@ let setHeightOfMain = (() => {
 let navLinks = document.querySelectorAll(".header__nav .nav__item");
 
 function checkActivePage() {
+    console.log(navLinks)
     for (let i = 0; i < navLinks.length; i++) {
+        console.log(navLinks[i].dataset.page)
         if (navLinks[i].dataset.page) {
             if (navLinks[i].dataset.page === pageLink) {
                 navLinks[i].classList.add("active");
